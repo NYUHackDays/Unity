4642e20a80880bb161cbe91ae6212738894b23d9
Abhi Agarwal <me@abhiagarwal.com>
2014-10-03 12:55:57 -0400

    Starting position

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HelloWorld : MonoBehaviour {
+
+	public List<string> list = new List<string>();
+	public float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+		print (list [0] + " " + list [1]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (speed, 0, transform.position.z) * Time.deltaTime);
+	}
+}
